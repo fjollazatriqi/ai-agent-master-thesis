06651d6511493c9b96de779c2c341cbe312b1c66
Fjolla Zatriqi <fjollazatriqi97@gmail.com>
2026-01-17 12:09:43 +0100

    deleted

--- src/example_issue30.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-def add_numbers(a, b):
-    return a + b
-=======
-# TODO: Implement issue #30: Create a function that adds two numbers
->>>>>>> 39cdaa779e58d077b8b104127d05999b82c4f4e9
